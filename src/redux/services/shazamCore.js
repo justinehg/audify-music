69ffe9dd5ad6aa4c8688d3e4cc2dce64294af8f3
Your Name--global user.email justinehoang21@gmail.com <you@example.com>
2022-11-22 01:55:51 -0600

    update existing files

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -11,7 +11,7 @@ export const shazamCoreApi = createApi({
         },
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () => '/charts/world?query=metallica' }),
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
     
     })
 });
