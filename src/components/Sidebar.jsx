004710cdd0d4d9fcfd74ed0c5958c1b04a33d6af
Your Name--global user.email justinehoang21@gmail.com <you@example.com>
2022-11-28 00:53:01 -0600

    created Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,25 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { RiCloseLine } from 'react-icons/ri';
+
+import { logo } from '../assets';
+import { links } from '../assets/constants';
+
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+    <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-gradient-to-br from-[#000001] to-[#04001c]">
+      <img src={logo} alt="logo" className="w-full h-23 object-contain"/>
+
+    </div>
+    </>
+  )
+    
+};
 
 export default Sidebar;
+
+// 1:09:02
